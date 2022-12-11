c4574f0896355aff91f41801f45d301796618ecc
Alex Koval <301019a@gmail.com>
2022-12-11 11:29:34 +0200

    task 2
    fin

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,35 +9,28 @@ const dataMinutes = document.querySelector('[data-minutes]');
 const dataSeconds = document.querySelector('[data-seconds]');
 let chosenTime = null;
 
+startBtn.addEventListener('click', startTimer);
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  // Вибір дати
-  // Метод onClose() з об'єкта параметрів викликається щоразу під час закриття елемента інтерфейсу, який створює flatpickr.
-  // Саме у ньому варто обробляти дату, обрану користувачем. Параметр selectedDates - це масив обраних дат, тому ми беремо перший елемент.
+
   onClose(selectedDates) {
-    chosenTime = selectedDates[0];
-    if (chosenTime.getTime() < Date.now()) {
+    chosenTime = selectedDates[0].getTime();
+    if (chosenTime < Date.now()) {
       window.alert('Please choose a date in the future');
     }
   },
   onChange(selectedDates) {
-    // Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
-    // Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
-    if (selectedDates[0].getTime() < Date.now()) {
-      startBtn.disabled = true;
-      // console.log(currentTime);
-    } else {
-      startBtn.disabled = false;
-    }
+    selectedDates[0].getTime() < Date.now()
+      ? (startBtn.disabled = true)
+      : (startBtn.disabled = false);
   },
 };
 
 flatpickr('#datetime-picker', options);
-
-startBtn.addEventListener('click', startTimer);
 
 function startTimer() {
   getDeltaTime();
@@ -45,13 +38,9 @@ function startTimer() {
 
 function getDeltaTime() {
   timerId = setInterval(() => {
-    const deltaTime = chosenTime.getTime() - Date.now();
+    const deltaTime = chosenTime - Date.now();
     const deltaTimeMs = convertMs(deltaTime);
-    if (deltaTime <= 0) {
-      clearInterval(timerId);
-    } else {
-      updateClockView(deltaTimeMs);
-    }
+    deltaTime <= 0 ? clearInterval(timerId) : updateClockView(deltaTimeMs);
   }, 1000);
 }
 
@@ -64,9 +53,7 @@ function convertMs(ms) {
   const days = addLeadingZero(Math.floor(ms / day));
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
   return { days, hours, minutes, seconds };
 }
 
