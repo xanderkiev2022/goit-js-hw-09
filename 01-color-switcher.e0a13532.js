28a2d221156d6ade33155ee9a315cb68c5bdfc7b
xanderkiev2022 <113243861+xanderkiev2022@users.noreply.github.com>
2022-12-11 10:15:42 +0000

    Deploying to gh-pages from @ xanderkiev2022/goit-js-hw-09@1c2234e4eb088ef3fde48e52c48fcaebd942dc9a 🚀

---
+++ 01-color-switcher.e0a13532.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0,d.disabled=!1}),1e3)})),d.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,d.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.e0a13532.js.map
